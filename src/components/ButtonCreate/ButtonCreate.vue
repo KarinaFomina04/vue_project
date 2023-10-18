f1eb835c3c01af0b4c1a32ce9cbc4e3448525df0
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-10-19 00:34:02 +0200

    Initial commit

---
+++ src/components/ButtonCreate/ButtonCreate.vue
@@ -0,0 +1,68 @@
+<template>
+  <button class="button-create" @click="handleClick" :disabled="isLoading">
+    <span v-if="isLoading" class="loader"></span>
+    <span v-else>Создать</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonCreate',
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      // Включите состояние загрузки после нажатия на кнопку
+      this.isLoading = true;
+
+      // Здесь можно выполнить асинхронные операции, например, запрос на сервер
+
+      // Затем отключите состояние загрузки, когда операция завершится
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 3000); // Пример задержки на 3 секунды
+    },
+  },
+};
+</script>
+
+<style scoped>
+.button-create {
+  border: 1px solid lightgray;
+  border-radius: 3px;
+  background-color: white;
+  color: black;
+  font-family: Arial, sans-serif;
+  padding: 8px 16px;
+  cursor: pointer;
+  outline: none;
+  position: relative; /* Для позиционирования прелоадера */
+}
+
+.button-create:hover {
+  background-color: dodgerblue;
+  color: white;
+}
+
+.loader {
+  border: 2px solid rgba(0, 0, 0, 0.2);
+  border-top: 2px solid dodgerblue;
+  border-radius: 50%;
+  width: 16px;
+  height: 16px;
+  animation: spin 1s linear infinite;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
---
+++ src/components/Dropdown/Dropdown.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="dropdown">
+    <input
+        v-model="selectedItem"
+        @click="toggleDropdown"
+        placeholder="Выберите элемент"
+    />
+    <ul v-if="isOpen" class="dropdown-list">
+      <li v-for="item in items" :key="item" @click="selectItem(item)">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dropdown',
+  data() {
+    return {
+      isOpen: false,
+      selectedItem: '',
+      items: ['Элемент 1', 'Элемент 2', 'Элемент 3', 'Элемент 4'], // Замените на свои элементы
+    };
+  },
+  methods: {
+    toggleDropdown() {
+      this.isOpen = !this.isOpen;
+    },
+    selectItem(item) {
+      this.selectedItem = item;
+      this.isOpen = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+input {
+  padding: 8px;
+}
+
+.dropdown-list {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  border: 1px solid lightgray;
+  background-color: white;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  max-height: 150px;
+  overflow-y: auto;
+}
+
+li {
+  padding: 8px;
+  cursor: pointer;
+}
+
+li:hover {
+  background-color: #f0f0f0;
+}
+</style>
